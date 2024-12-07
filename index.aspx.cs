bec9371314b1c3e806bf179c26b0df3678297510
Juaanyzy <juanaragon01011@gmail.com>
2024-12-07 08:33:16 -0500

    Update index.aspx.cs
    testing

--- index.aspx.cs
+++ index.aspx.cs
@@ -15,3 +15,23 @@ namespace MelodyHub
         }
     }
 }
+
+namespace MelodyHub
+{
+    public partial class Inicio : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["nombre"] == null)
+            {
+                Response.Redirect("Index.aspx");
+            }
+        }
+        protected void BtnSalir_Click1(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("login.aspx");
+            Context.ApplicationInstance.CompleteRequest();
+        }
+    }
+}
